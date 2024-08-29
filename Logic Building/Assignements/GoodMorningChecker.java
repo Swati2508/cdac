7a6afec4c7c880cc646f47130b84e41b1190c862
Swati2508 <97654696+Swati2508@users.noreply.github.com>
2024-08-29 16:23:37 +0530

    Add files via upload

---
+++ Logic Building/Assignements/GoodMorningChecker.java
@@ -0,0 +1,8 @@
+public class GoodMorningChecker {
+    public static void main(String[] args) {
+        int hour = 8; 
+        if (hour >= 5 && hour < 12) {
+            System.out.println("Good Morning!");
+        }
+    }
+}
---
+++ Logic Building/Assignements/NegativeNumberChecker.java
@@ -0,0 +1,10 @@
+public class NegativeNumberChecker {
+    public static void main(String[] args) {
+        int number = -10; 
+        if (number < 0) {
+            System.out.println(number + " is negative.");
+        } else {
+            System.out.println(number + " is not negative.");
+        }
+    }
+}
