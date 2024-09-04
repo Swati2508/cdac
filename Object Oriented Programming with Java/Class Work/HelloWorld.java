aa57cadc6756460ae994d5e3ceadc8178ec17568
Swati2508 <97654696+Swati2508@users.noreply.github.com>
2024-09-04 11:03:11 +0530

    Add files via upload

---
+++ Object Oriented Programming with Java/Class Work/HelloWorld.java
@@ -0,0 +1,8 @@
+// your first program
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    
+}          
---
+++ Object Oriented Programming with Java/Class Work/Program.java
@@ -0,0 +1,6 @@
+class Program{ //file name and class name should be same
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+}
